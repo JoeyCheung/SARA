7c1f5ab0d87e74ad5322d1d974345e435b5897e6
David Dataram <daviddataram@Davids-MacBook-Pro-3.local>
2019-03-17 11:51:48 -0400

    Fixed browser info and fixed info on search page

--- scripts/script.js
+++ scripts/script.js
@@ -31,21 +31,36 @@ window.onload = function(){
    let screen = document.getElementById('screen-info');
    let location = document.getElementById('location-info');
    let navigator = document.getElementById('navigator-info');
+   let geolocation = document.getElementById('geolocation-info');
    let el = document.createElement('p');
-   el.innerHTML = "Screen: " + window.screen.height + "px X " + window.screen.width + "px";
+   el.innerHTML = "Height X Width: " + window.screen.height + "px X " + window.screen.width + "px <br>"
+                + "Available height X width: " + window.screen.availHeight + "px X " + window.screen.availWidth + "px <br>"
+                + "Color Depth: " + window.screen.colorDepth + "<br>"
+                + "Pixel Depth: " + window.screen.pixelDepth + "<br"
+                + "Window Height X Width: " + window.innerHeight + "px X " + window.innerWidth + "px";
    screen.appendChild(el);
    el = document.createElement('p');
-   el.innerHTML = "Screen: " + window.screen.availHeight + "px X " + window.screen.availWidth + "px";
-   screen.appendChild(el);
-   el = document.createElement('p');
-   el.innerHTML = "href: " + window.location.href;
+   el.innerHTML = "href: " + window.location.href + "<br>"
+                + "Hostname: " + window.location.hostname + "<br>"
+                + "Pathname: " + window.location.pathname + "<br>"
+                + "Protocol: " + window.location.protocol + "<br>"
    location.appendChild(el);
    el = document.createElement('p');
-   el.innerHTML = "App Name: " + window.navigator.appName;
+   el.innerHTML = "App Name: " + window.navigator.appName + "<br>"
+                + "App Version: " + window.navigator.appVersion + "<br> "
+                + "Product: " + window.navigator.product + "<br> "
+                + "userAgent: " + window.navigator.userAgent + "<br> "
+                + "Platform: " + window.navigator.platform + "<br> "
+                + "Language: " + window.navigator.languages;
    navigator.appendChild(el);
    el = document.createElement('p');
-   el.innerHTML = "App Version: " + window.navigator.appVersion;
-   navigator.appendChild(el);
+   window.navigator.geolocation.getCurrentPosition(function(coords){
+    el.innerHTML = "Latitude: " + coords.coords.latitude + "<br>"
+    + "Longitude: " + coords.coords.longitude;
+   }, function(err){
+       el.innerHTML = "Geolocation not supported!";
+   });
+   geolocation.appendChild(el);
    
 }
 /*
